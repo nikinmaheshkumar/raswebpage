1b0e33012a08137b5bd66eb293e8e947e865067d
Nikin Mahesh Kumar <nikinmaheshkumar@gmail.com>
2025-06-13 15:48:36 +0530

    refactor: reset project structure to start from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "./components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import Footer from "./components/Footer";
-
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import './styles/App.css';
+import { motion } from "framer-motion";
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
-    <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Home scrollToElement="about" />} />
-          <Route path="/event" element={<Home scrollToElement="events"/>} />
-          <Route path="/blog" element={<Home scrollToElement="blogs" />} />
-          <Route path="/board" element={<Home scrollToElement="board"/>} />
-          <Route path="/gallery" element={<Home scrollToElement="gallery"/>} />
-          <Route path="/contact" element={<Home scrollToElement="contact"/>} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
-
-
-        <Footer />
-        
-
-      </div>
-    </Router>
-  );
+    <>
+      <motion.div
+      initial={{opacity: 0, y: -50}}
+      animate={{opacity: 1, y: 0}}
+      transition={{duration: 0.5}}
+    >
+      Hello, Framer Motion! 👋
+    </motion.div>
+    </>
+  )
 }
 
-export default App;
+export default App
